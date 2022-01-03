3a17958441d128a2a47cb8d2d191d316c296fb7d
souporserious <tarnold@frame.io>
2022-01-03 00:06:25 -0800

    fix cli formatting

--- cli/App.tsx
+++ cli/App.tsx
@@ -7,11 +7,98 @@ import { Option, Select } from './components/Select'
 import { getTopLevelDirectories } from './utils/get-top-level-directories'
 
 const directories = getTopLevelDirectories()
+const formatString = (filePath, text) =>
+  dprint.format(filePath, text, {
+    quoteStyle: 'alwaysSingle',
+    semiColons: 'asi',
+    newLineKind: 'system',
+    lineWidth: 80,
+    indentWidth: 2,
+  })
 
-function NewFile() {
+function NewFile({ onAdd }: { onAdd: () => void }) {
   const [directory, setDirectory] = useState(null)
   const [value, setValue] = useState('')
   const [message, setMessage] = useState(null)
+
+  function handleSubmit() {
+    try {
+      /** Create index file that exports modules and types. */
+      const indexSourceFile = project.createSourceFile(
+        `${directory}/${value}/index.ts`,
+        `export * from './${value}'`
+      )
+
+      /** Create initial file for exports based on the value. */
+      const mainSourceFile = project.createSourceFile(
+        `${directory}/${value}/${value}.tsx`
+      )
+
+      mainSourceFile
+        .addFunction({
+          name: value,
+          isExported: true,
+        })
+        .setBodyText(`return <div>Hello ${value}</div>`)
+
+      /** Create a README with some initial content. */
+      project.createSourceFile(
+        `${directory}/${value}/README.mdx`,
+        `Hello ${value}\n`
+      )
+
+      /** Update the local index.ts file to a barrel export and sort */
+      const rootIndexSourceFile = project.getSourceFile(`${directory}/index.ts`)
+
+      rootIndexSourceFile
+        .addExportDeclaration({
+          moduleSpecifier: `./${value}`,
+          namedExports: [value],
+        })
+        .toNamespaceExport()
+
+      /** Sort export declarations alphabetically. */
+      const exportDeclarations = rootIndexSourceFile.getExportDeclarations()
+      const sortedDeclarations = exportDeclarations
+        .map((declaration) =>
+          declaration.getModuleSpecifier().getLiteralValue()
+        )
+        .sort()
+
+      exportDeclarations.forEach((declaration) => {
+        const sortedIndex = sortedDeclarations.findIndex((moduleFilePath) => {
+          return (
+            moduleFilePath ===
+            declaration.getModuleSpecifier().getLiteralValue()
+          )
+        })
+        declaration.setOrder(sortedIndex)
+      })
+
+      /** Lastly, format all files. */
+      ;[mainSourceFile, indexSourceFile, rootIndexSourceFile].forEach(
+        (sourceFile) => {
+          sourceFile.replaceWithText(
+            formatString(sourceFile.getFilePath(), sourceFile.getFullText())
+          )
+        }
+      )
+
+      project.save()
+
+      setMessage(`Created ${value} in ${directory} successfully!`)
+      setTimeout(() => {
+        onAdd()
+      }, 3000)
+    } catch {
+      setMessage('Name already in use, try another name.')
+      setTimeout(() => {
+        setMessage(null)
+      }, 3000)
+    } finally {
+      setValue('')
+    }
+  }
 
   if (directory === null) {
     return (
@@ -31,89 +118,7 @@ function NewFile() {
         <Box marginRight={1}>
           <Text>Enter name:</Text>
         </Box>
-        <TextInput
-          value={value}
-          onChange={setValue}
-          onSubmit={() => {
-            try {
-              /** Create index file that exports modules and types. */
-              project.createSourceFile(
-                `${directory}/${value}/index.ts`,
-                `export * from './${value}'`
-              )
-
-              /** Create initial file for exports based on the value. */
-              project
-                .createSourceFile(`${directory}/${value}/${value}.tsx`)
-                .addFunction({
-                  name: value,
-                  isExported: true,
-                })
-                .setBodyText(`return <div>Hello ${value}</div>`)
-
-              /** Create a README with some initial content. */
-              project.createSourceFile(
-                `${directory}/${value}/README.mdx`,
-                `Hello ${value}`
-              )
-
-              /** Update the local index.ts file to a barrel export and sort */
-              const indexSourceFile = project.getSourceFile(
-                `${directory}/index.ts`
-              )
-
-              indexSourceFile
-                .addExportDeclaration({
-                  moduleSpecifier: `./${value}`,
-                  namedExports: [value],
-                })
-                .toNamespaceExport()
-
-              /** Lastly, sort export declarations alphabetically. */
-              const exportDeclarations = indexSourceFile.getExportDeclarations()
-              const sortedDeclarations = exportDeclarations
-                .map((declaration) =>
-                  declaration.getModuleSpecifier().getLiteralValue()
-                )
-                .sort()
-
-              exportDeclarations.forEach((declaration) => {
-                const sortedIndex = sortedDeclarations.findIndex(
-                  (moduleFilePath) => {
-                    return (
-                      moduleFilePath ===
-                      declaration.getModuleSpecifier().getLiteralValue()
-                    )
-                  }
-                )
-                declaration.setOrder(sortedIndex)
-              })
-
-              project.save()
-
-              /** Format the file now that we've saved our transformations. */
-              const formattedSourceFile = dprint.format(
-                indexSourceFile.getFilePath(),
-                indexSourceFile.getFullText(),
-                {
-                  quoteStyle: 'alwaysSingle',
-                  semiColons: 'asi',
-                }
-              )
-
-              indexSourceFile.replaceWithText(formattedSourceFile)
-
-              setMessage(`Created ${value} in ${directory} successfully!`)
-            } catch {
-              setMessage('Name already in use, try another name.')
-            } finally {
-              setValue('')
-              setTimeout(() => {
-                setMessage(null)
-              }, 3000)
-            }
-          }}
-        />
+        <TextInput value={value} onChange={setValue} onSubmit={handleSubmit} />
       </Box>
       {message && <Text>{message}</Text>}
     </Box>
@@ -124,7 +129,7 @@ export default function App() {
   const [selectedToken, setSelectedToken] = useState(null)
 
   if (selectedToken === 'new') {
-    return <NewFile />
+    return <NewFile onAdd={() => setSelectedToken(null)} />
   }
 
   return (
