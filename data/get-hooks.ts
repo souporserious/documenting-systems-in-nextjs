4be58e9b8dd18f4a5ef080bdec08408eb67fd6f5
souporserious <tarnold@frame.io>
2022-01-07 22:24:43 -0800

    use ts morph to gather hook docs

--- data/get-hooks.ts
+++ data/get-hooks.ts
@@ -1,66 +1,50 @@
-import { readdirSync } from 'fs'
-import * as path from 'path'
-import { camelCase } from 'case-anything'
+import { kebabCase } from 'case-anything'
 import { Node } from 'ts-morph'
 import { transformCode } from './transform-code'
 import { hooksSourceFile } from './project'
 
 export async function getHooks() {
-  const hooksDirectory = hooksSourceFile.getDirectoryPath()
-  const hooks = readdirSync(hooksDirectory).filter(
-    (file) => !file.startsWith('index')
-  )
-  let docs = {}
+  return Promise.all(
+    Array.from(hooksSourceFile.getExportedDeclarations())
+      .map(async ([name, [declaration]]) => {
+        if (!Node.isFunctionDeclaration(declaration)) {
+          return null
+        }
 
-  hooksSourceFile.getExportedDeclarations().forEach(([declaration]) => {
-    if (Node.isFunctionDeclaration(declaration)) {
-      const [doc] = declaration.getJsDocs()
+        const path = declaration.getSourceFile().getFilePath()
+        const [doc] = declaration.getJsDocs()
+        let description = null
+        let examples = []
 
-      if (doc) {
-        docs[declaration.getName()] = {
-          description: doc.getComment(),
-          examples: doc
+        if (doc) {
+          description = doc.getComment()
+          examples = doc
             .getTags()
             .filter((tag) => tag.getTagName() === 'example')
-            .map((tag) => tag.getCommentText()),
+            .map((tag) => tag.getCommentText())
         }
-      }
-    }
-  })
 
-  return Promise.all(
-    hooks.map(async (fileName) => {
-      const extension = path.extname(fileName).slice(1)
-      const name = path.basename(fileName, `.${extension}`)
-      const doc = docs[camelCase(name)]
-      const hookPath = path.resolve(hooksDirectory, fileName)
-      let hookExamples = doc.examples
+        if (examples) {
+          const compiledExamples = await Promise.all(
+            examples.map(transformCode)
+          )
+          examples = examples.map((codeString, index) => ({
+            code: codeString,
+            compiledCode: compiledExamples[index],
+          }))
+        }
 
-      if (hookExamples) {
-        const compiledExamples = await Promise.all(
-          hookExamples.map((codeString) => {
-            const reactImport = `import React from 'react'\n`
-            return transformCode(reactImport + codeString)
-          })
-        )
-        hookExamples = hookExamples.map((codeString, index) => ({
-          code: codeString,
-          compiledCode: compiledExamples[index],
-        }))
-      } else {
-        hookExamples = null
-      }
-
-      return {
-        name: camelCase(name),
-        slug: name,
-        description: doc.description,
-        examples: hookExamples,
-        path:
-          process.env.NODE_ENV === 'development'
-            ? hookPath
-            : hookPath.replace(process.cwd(), ''),
-      }
-    })
+        return {
+          description,
+          examples,
+          name,
+          slug: kebabCase(name),
+          path:
+            process.env.NODE_ENV === 'development'
+              ? path
+              : path.replace(process.cwd(), ''),
+        }
+      })
+      .filter(Boolean)
   )
 }
