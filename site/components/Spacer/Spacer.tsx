02c015e19bdb82b7efc118a2cd612b2958d06d3c
Travis Arnold <ftntravis@gmail.com>
2022-12-03 14:13:36 -0800

    fix server warning

--- site/components/Spacer/Spacer.tsx
+++ site/components/Spacer/Spacer.tsx
@@ -4,9 +4,7 @@ interface Props {
   size?: string
 }
 
-export const Spacer = styled.div.attrs({ 'aria-hidden': true })<Props>(
-  ({ size }) => ({
-    gridColumn: '1 / -1',
-    minHeight: size,
-  })
-)
+export const Spacer = styled.div<Props>(({ size }) => ({
+  gridColumn: '1 / -1',
+  minHeight: size,
+}))
