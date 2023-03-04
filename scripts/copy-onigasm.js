28519ce174bb35ed6f9bb009b9d9488fd589ebcc
Travis Arnold <ftntravis@gmail.com>
2023-03-04 15:13:09 -0800

    remove onigasm copy script

--- scripts/copy-onigasm.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-const lib = path.join(__dirname, '../node_modules/onigasm/lib/onigasm.wasm')
-const public = path.join(__dirname, '../public/onigasm.wasm')
-
-/** Copy onigasm wasm bin so we can load through fetch instead of Webpack. */
-fs.copyFileSync(lib, public)
